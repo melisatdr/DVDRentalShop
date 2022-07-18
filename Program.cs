7af7976ea54c14c71e24e2d1c466ece0cfb822de
Melisa Tunçdemir <94766611+melisatdr@users.noreply.github.com>
2022-07-19 01:22:59 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using DVDRentalShop.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVDRentalShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new Menu().Start();
+            Console.ReadKey();
+
+        }
+    }
+}
